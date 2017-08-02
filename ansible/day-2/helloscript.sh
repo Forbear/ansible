a4da13efcd5b1cfabf7fd7051d94c405a0a8e356
Forbear <silnyagindan@gmail.com>
2017-08-02 14:26:07 +0300

    Tomcat installed by role with variables

---
+++ ansible/day-2/helloscript.sh
@@ -0,0 +1,2 @@
+# /usr/bin/
+CATALINA_HOME=/opt/tomcat/8.5.9/
---
+++ ansible/day-2/script.sh
@@ -0,0 +1,6 @@
+mkdir -p .ssh/
+mkdir -p /opt/tomcat/
+chmod 755 /opt/tomcat/
+cat key.pub >> ./.ssh/authorized_keys
+chmod 700 .ssh
+chmod 600 .ssh/authorized_keys
