046f8edd086a2c4e5bd3fdc0325d04497f704aec
Forbear <silnyagindan@gmail.com>
2017-08-03 07:58:59 +0300

    Varification about to complete

--- ansible/day-2/script.sh
+++ ansible/day-2/script.sh
@@ -3,6 +3,7 @@ mkdir -p /opt/tomcat/
 mkdir -p /etc/ansible/facts.d/
 echo "[general]" > /etc/ansible/facts.d/java.fact
 echo "[general]" > /etc/ansible/facts.d/nginx.fact
+echo "[general]" > /etc/ansible/facts.d/tomcat.fact
 chmod 755 /opt/tomcat/
 cat key.pub >> ./.ssh/authorized_keys
 chmod 700 .ssh
